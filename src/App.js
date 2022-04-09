60bac3326a254ca2719d5395bc518a474ec9b753
Tejasvin <tejasvinn.vimal@gmail.com>
2022-04-09 23:40:59 +0530

    Config files updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Route } from 'react-router-dom';
-
+import { Route, Routes} from 'react-router-dom';
+ 
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
@@ -8,16 +8,18 @@ function App() {
   //localhost:3000/path
  
   return (
-    <div>
-     <Route path="/">
-       <AllMeetupsPage />
+    <div> 
+     <Routes>   
+     <Route path="/" element={AllMeetupsPage}>
+       
      </Route> 
-     <Route path='/new-meetup'>
-       <NewMeetupPage />
+     <Route path='/new-meetup' element={NewMeetupPage}>
+      
      </Route> 
-     <Route path='/favorites'>
-       <FavoritesPage />
-     </Route>    
+     <Route path='/favorites' element={FavoritesPage}>
+      
+     </Route> 
+     </Routes>   
     </div>
     
   );  
